252df023ea1df71e7fa7182d61e783a9a43b5289
JoycePedrozo <joycepedrozo1@gmail.com>
2024-08-07 14:15:05 -0300

    commit

--- EstruturaDeDecisao_002/atividade011.py
+++ EstruturaDeDecisao_002/atividade011.py
@@ -1,5 +1,5 @@
 """
-11- As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contraram para desenvolver o programa que calculará os reajustes.
+11- As Organizações Tabajara resolveram dar um aumento de salário aos seus colaboradores e lhe contrataram para desenvolver o programa que calculará os reajustes.
 Faça um programa que recebe o salário de um colaborador e o reajuste segundo o seguinte critério, baseado no salário atual:
     -salários até R$ 280,00 (incluindo) : aumento de 20%
     -salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
@@ -10,3 +10,27 @@ Faça um programa que recebe o salário de um colaborador e o reajuste segundo o
         - O valor do aumento;
         - O novo salário, após o aumento.
 """
+salario = float(input("Digite o salário do colaborador: R$ "))
+
+print(f'O salário atual: -------------{salario}')
+
+if salario <= 280.00:
+    salarioN = (salario * 20)/100
+    print(f'O percentual de aumento aplicado de 20% em cima do valor do salário é de: {salarioN}')
+    print(f'O seu salário novo é de : ------------{salarioN + salario}')
+
+elif salario <= 700.00:
+    salarioN = (salario * 15)/100
+    print(f'O percentual de aumento aplicado de 15% em cima do valor do salário é de: {salarioN}')
+    print(f'O seu salário novo é de : ------------{salarioN + salario}')
+
+elif salario < 1500.00:
+    salarioN = (salario * 10)/100
+    print(f'O percentual de aumento aplicado de 10% em cima do valor do salário é de: {salarioN}')
+    print(f'O seu salário novo é de : ------------{salarioN + salario}')
+
+else :
+    salarioN = (salario * 5)/100
+    print(f'O percentual de aumento aplicado de 15% em cima do valor do salário é de: {salarioN}')
+    print(f'O seu salário novo é de : ------------{salarioN + salario}')
+
